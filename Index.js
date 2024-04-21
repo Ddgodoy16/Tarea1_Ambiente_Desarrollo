8a08e2a1caf6577194d3be3e20927c7709365391
Ddgodoy16 <elvinnu_es25@outlook.es>
2024-04-21 16:51:18 -0500

    Codigo Completo

---
+++ Index.js
@@ -0,0 +1,9 @@
+var colors = require("colors");
+
+
+function suma(num1, num2, num3) {
+    return num1 + num2 * num3;
+  }
+  
+  console.log("El resultado es:".bgGreen)
+  console.log(suma(5, 3, 4).toString().red.underline)
